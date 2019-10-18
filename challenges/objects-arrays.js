7314b7976a99299abdc2d81170e1702fa3a0245f
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2019-10-18 10:10:52 -0700

    corrected error in displayNames array

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -125,7 +125,7 @@ The zoos want to display both the scientific name and the animal name in front o
 */
 const displayNames = [];
 zooAnimals.forEach( (animal) => {
-  displayNames.push('Name: ${animal.animal_name}, Scientific: ${animal.scientific_name}');
+  displayNames.push(`Name: ${animal.animal_name}, Scientific: ${animal.scientific_name}`);
 });
 console.log(displayNames);
 
